b503b1366baedeca3df3d4e012746ebecd62476c
Marvelous <marvellousolorunwa23@gmail.com>
2025-01-22 20:57:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const emailError = document.getElementById('error-message');
 form.addEventListener("submit", e => {
     e.preventDefault();
 
-    if(email.innerText === ""){
+    if(email.innerText == ""){
         emailError.style.display = "block";
         email.classList.add("error");
     } else {
