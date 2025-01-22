7e17169ca193bc2ef8dab5233db3b0bbbd079e56
Marvelous <marvellousolorunwa23@gmail.com>
2025-01-22 21:27:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const form = document.querySelector('form');
 const email = document.getElementById("email");
 const emailError = document.getElementById('error-message');
+const emailValue = email.value.trim();
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 form.addEventListener("submit", e => {
     e.preventDefault();
 
-    if(email.textContent === ""){
-        console.log('input a value');
+    // if(email.textContent === ""){
+    //     console.log('input a value');
         // emailError.style.display = "block";
         // email.classList.add("error");
-    }else{
-        console.log('correct')
-    };
+    // }else{
+    //     console.log('correct')
+    // };
     // if (email.innerText != ""){
     //     console.log("correct");
     //     emailError.style.display = "none";
     // }
+
+    if(!emailValue || !emailRegex.test(emailValue)){
+        emailError.style.display= 'block';
+        email.classList.add('error');
+    } else{
+        emailError.style.display = "none";
+        email.classList.remove('error');
+        alert('Your Email has be submitted')
+    };
 });
